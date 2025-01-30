309daf8a73908972c78cf28f4f91c303940e4b6c
Gerrit Balindt <gerrit.balindt@gruenecho.de>
2025-01-30 18:20:08 +0100

    test build

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '/FUK-TB-web' : '/FUK-TB-web'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
